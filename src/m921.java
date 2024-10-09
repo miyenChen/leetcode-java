e1c0951853cec4d4424ecc46d9c77549a8a0bb25
yuminc <jorg77jorg77@gmail.com>
2024-10-09 11:33:03 +0800

    Update:921-add second solution

--- src/m921.java
+++ src/m921.java
@@ -2,25 +2,43 @@ import java.util.Stack;
 
 public class m921 {
     public static int minAddToMakeValid(String s) {
-        Stack<Character> stack = new Stack<>();
+        // 使用stack方法
+        // Stack<Character> stack = new Stack<>();
+        //
+        // for (int i = 0; i < s.length(); i++) {
+        //     char ch = s.charAt(i);
+        //
+        //     if (ch == '(' || stack.isEmpty()) {
+        //         stack.push(ch);
+        //     } else {
+        //         // 閉括號
+        //         char el = stack.peek();
+        //         if (el == '(') {
+        //             stack.pop();
+        //         } else {
+        //             stack.push(ch);
+        //         }
+        //     }
+        // }
+        //
+        // return stack.size();
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-
-            if (ch == '(' || stack.isEmpty()) {
-                stack.push(ch);
-            } else {
-                // 閉括號
-                char el = stack.peek();
-                if (el == '(') {
-                    stack.pop();
-                } else {
-                    stack.push(ch);
+        // 計算數量
+        char[] charArr=s.toCharArray();
+        int open=0;
+        int close =0;
+        for(char ch:charArr){
+            if(ch=='('){
+                open++;
+            }else{
+                if(open>0){
+                    open--;
+                }else {
+                    close++;
                 }
             }
         }
-
-        return stack.size();
+        return open+close;
     }
 
     public static void main(String[] args) {
